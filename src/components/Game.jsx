e9a2f920a56d8e926061d309d402a741d33e2f5a
lfioreetchevest@gmail.com <lfioreetchevest@gmail.com>
2023-12-27 16:37:01 -0300

    Update JSON and css

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -7,7 +7,7 @@ export default function Producto ({image, title, new_price, free_ship}) {
         <div className="producto">
             <img src={image} alt="" />
             <h3 className="product-title">{title}</h3>
-            <p className="price"><i class="bi bi-star-fill"></i> {new_price}</p>
+            <p className="rate"><i class="bi bi-star-fill"></i> {new_price}</p>
             <span className="ship">{free_ship}</span>
         </div>
     )
--- src/components/Pages/History.jsx
+++ src/components/Pages/History.jsx
@@ -3,7 +3,7 @@ import GrillaGames from "../GrillaGames";
 
 export const History = () => {
   return (
-    <div>
+    <div className="history">
       <h1>History of Grand Theft Auto</h1>
       <p>All the Grand Theft Auto Games</p>
       <GrillaGames/>
