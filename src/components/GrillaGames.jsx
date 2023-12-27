bd0ec4272504503feb4ca5956bf139281d845473
lfioreetchevest@gmail.com <lfioreetchevest@gmail.com>
2023-12-27 00:10:53 -0300

    Add swiper js to about section

--- src/components/GrillaGames.jsx
+++ src/components/GrillaGames.jsx
@@ -1,21 +1,51 @@
+import React, { useRef, useState } from 'react';
 import './GrillaGames.css';
-import Games from '../data/games.json'
+import Games from '../data/games.json';
 import Game from './Game';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css/pagination'; 
+import 'swiper/css/free-mode';
+import 'swiper/css'; 
+import { Autoplay, Mousewheel, Pagination } from 'swiper/modules';
 
-export default function GrillaGames () {
-
+export default function GrillaProductos() {
     return (
-        <div className='grilla'>
-            {Games.map((producto,index) => (
-                <Game 
-                    key={index}
-                    image = {producto.image}
-                    title = {producto.title}
-                    new_price = {producto.new_price}
-                    free_ship = {producto.free_ship}
+        <Swiper
+            spaceBetween={10}
+            slidesPerView={4}
+            mousewheel={true}
+            autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+            }}    
+            pagination={{
+            clickable: true,
+            }}
+            modules={[Mousewheel, Pagination, Autoplay]}
+            loop
+            breakpoints={{
+                // Configuración para diferentes tamaños de pantalla
+                320: {
+                    slidesPerView: 1,
+                },
+                768: {
+                    slidesPerView: 2,
+                },
+                1024: {
+                    slidesPerView: 4,
+                },
+            }}
+        >
+            {Games.map((game, index) => (
+            <SwiperSlide className='swiper-games' key={index}>
+                <Game
+                    image={game.image}
+                    title={game.title}
+                    new_price={game.new_price}
+                    free_ship={game.free_ship}
                 />
+            </SwiperSlide>
             ))}
-            
-        </div>
-    )
+        </Swiper>
+    );
 }
