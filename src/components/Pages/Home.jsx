6ba4d1bffb3f012b3ed6e2054a6168ff18267b35
lfioreetchevest@gmail.com <lfioreetchevest@gmail.com>
2023-12-29 00:47:18 -0300

    cta home update

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -10,9 +10,10 @@ export const Home = () => {
       <h1 className="title">RELEASE DATE</h1>
       <CountdownTimer />
       <p className="release-date">COMING IN SEPTEMBER 2025</p>
-      <a href='https://www.rockstargames.com/reddeadredemption2' className="new-games" target="_blank" rel="noreferrer">
-        <i class="bi bi-arrow-right-circle"></i>
-        <img src="logo-rdd2.svg" alt="Logo Read Dead Redemption 2" />
+      <a href='/History' className="new-games">
+        <i class="bi bi-arrow-right-circle"></i> 
+        See all  
+        <img src="logo-gta.svg" alt="Logo Grand Theft Auto" />
       </a>
     </div>
   );
