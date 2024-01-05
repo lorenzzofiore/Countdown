62d78ed5a867209353d4a187f47cb610666361a9
lfioreetchevest@gmail.com <lfioreetchevest@gmail.com>
2024-01-05 20:52:12 -0300

    update

--- src/components/FormContact.jsx
+++ src/components/FormContact.jsx
@@ -1,32 +1,34 @@
-import './FormContact.css'
+import React from 'react';
+import './FormContact.css';
 
-export default function FormContact () {
-    return (
-        <main className='contact-main'>
-            {/* Este es el form oculto de Netlify */}
-            <form action="/Contact" name='contacto' hidden data-netlify='true' netlify-honeypot='bot-field'>
-                <input type="text" name='nombre' />
-                <input type="email" name='email' />
-                <textarea name='comentarios'></textarea>
-            </form>
+export default function FormContact() {
+  return (
+    <main className="contact-main">
+      {/* Formulario oculto de Netlify */}
+      <form name="contacto" hidden data-netlify="true" netlify-honeypot="bot-field">
+        <input type="text" name="nombre" />
+        <input type="email" name="email" />
+        <textarea name="comentarios"></textarea>
+      </form>
 
-            <form method="post" className="form" netlify>
-                <label>
-                    Name:
-                </label>
-                <input type="text" name='nombre' placeholder='Name' />
-                <label>
-                    E-mail:
-                </label>
-                <input type="email" name='email' placeholder='E-mail'/>
-                <label>
-                    Comments:
-                </label>
-                <textarea name='comentarios' placeholder='Leave your comments here'></textarea>
-                {/* Conectamos al form invisible */}
-                <input type="hidden" name='form-name' value='contacto' />
-                <input type="submit" value='Enviar' className='submit-btn'/>
-            </form>
-        </main>
-    )
+      {/* Formulario visible */}
+      <form method="post" className="form" action="/contact" data-netlify="true">
+        <label>
+          Name:
+        </label>
+        <input type="text" name="nombre" placeholder="Name" />
+        <label>
+          E-mail:
+        </label>
+        <input type="email" name="email" placeholder="E-mail" />
+        <label>
+          Comments:
+        </label>
+        <textarea name="comentarios" placeholder="Leave your comments here"></textarea>
+        {/* Conectamos al formulario invisible */}
+        <input type="hidden" name="form-name" value="contacto" />
+        <input type="submit" value="Enviar" className="submit-btn" />
+      </form>
+    </main>
+  );
 }
