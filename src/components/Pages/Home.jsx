23130b112f133456ba88b0e30391839523c39761
lfioreetchevest@gmail.com <lfioreetchevest@gmail.com>
2024-01-06 15:16:34 -0300

    update

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -10,7 +10,7 @@ export const Home = () => {
       <h1 className="title">RELEASE DATE</h1>
       <CountdownTimer />
       <p className="release-date">COMING IN SEPTEMBER 2025</p>
-      <a href='/history' className="new-games">
+      <a href='https://countdowngtavi.netlify.app/history' className="new-games">
         <i class="bi bi-arrow-right-circle"></i> 
         See all  
         <img src="logo-gta.svg" alt="Logo Grand Theft Auto" />
